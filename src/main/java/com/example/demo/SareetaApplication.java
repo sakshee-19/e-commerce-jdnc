8ab20437d809177495a27b940206ac33ec882090
saksjain <saksjain@adobe.com>
2021-01-28 13:51:46 +0530

    jwt authentication

--- src/main/java/com/example/demo/SareetaApplication.java
+++ src/main/java/com/example/demo/SareetaApplication.java
@@ -2,16 +2,22 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 //@EnableJpaRepositories("com.example.demo.model")
 //@EntityScan("com.example.demo.model.persistence")
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class}) // to exlcude auto security class and impl JWT
 public class SareetaApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SareetaApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SareetaApplication.class, args);
+    }
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
 }
